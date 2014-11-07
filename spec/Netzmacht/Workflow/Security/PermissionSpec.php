8a0a97d2d95471c6292a42e24b6b3a0a06574323
David Molineus <mail@netzmacht.de>
2014-11-07 13:20:59 +0100

    complete security specs

---
+++ spec/Netzmacht/Workflow/Security/PermissionSpec.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace spec\Netzmacht\Workflow\Security;
+
+use Netzmacht\Workflow\Flow\Workflow;
+use Netzmacht\Workflow\Security\Permission;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Class PermissionSpec
+ * @package spec\Netzmacht\Workflow\Security
+ * @mixin Permission
+ */
+class PermissionSpec extends ObjectBehavior
+{
+    function let(Workflow $workflow)
+    {
+        $workflow->getName()->willReturn('workflow');
+        $this->beConstructedThrough('forWorkflow', array($workflow, 'perm'));
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Netzmacht\Workflow\Security\Permission');
+    }
+
+    function it_has_a_workflow_name()
+    {
+        $this->getWorkflowName()->shouldReturn('workflow');
+    }
+
+    function it_has_a_permission_id()
+    {
+        $this->getPermissionId()->shouldReturn('perm');
+    }
+
+    function it_equals_if_workflow_and_permission_id_matches(Permission $permission)
+    {
+        $permission->__toString()->willReturn('workflow/perm');
+
+        $this->equals($permission)->shouldReturn(true);
+    }
+
+    function it_does_not_equals_if_not_the_same_workflow(Permission $permission)
+    {
+        $permission->__toString()->willReturn('workflow2/perm');
+
+        $this->equals($permission)->shouldReturn(false);
+    }
+
+    function it_casts_to_string()
+    {
+        $this->__toString()->shouldReturn('workflow/perm');
+    }
+
+    function it_reconstruct_form_string()
+    {
+        $this->beConstructedThrough('fromString', array('workflow/perm'));
+
+        $this->getWorkflowName()->shouldReturn('workflow');
+        $this->getPermissionId()->shouldReturn('perm');
+    }
+}
---
+++ spec/Netzmacht/Workflow/Security/UserSpec.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace spec\Netzmacht\Workflow\Security;
+
+use Netzmacht\Workflow\Security\Permission;
+use Netzmacht\Workflow\Security\User;
+use Netzmacht\Workflow\Security\Role;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+/**
+ * Class UserSpec
+ * @package spec\Netzmacht\Workflow\Acl
+ * @mixin User
+ */
+class UserSpec extends ObjectBehavior
+{
+    function let(Role $role)
+    {
+        $role->equals($role)->willReturn(true);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Netzmacht\Workflow\Security\User');
+    }
+
+    function it_assigns_a_role(Role $role)
+    {
+        $this->assign($role)->shouldReturn($this);
+        $this->hasRole($role)->shouldReturn(true);
+    }
+
+    function it_knows_if_role_is_assigned(Role $role)
+    {
+        $this->hasRole($role)->shouldReturn(false);
+    }
+
+    function it_rejects_role(Role $role)
+    {
+        $this->assign($role);
+        $this->hasRole($role)->shouldReturn(true);
+        $this->reject($role);
+        $this->hasRole($role)->shouldReturn(false);
+    }
+
+    function it_checks_permission(Role $role, Permission $permission)
+    {
+        $this->assign($role);
+        $role->hasPermission($permission)->willReturn(true);
+
+        $this->hasPermission($permission)->shouldReturn(true);
+    }
+
+    function it_checks_permission_until_found(Role $role, Permission $permission, Role $roleB)
+    {
+        $this->assign($roleB);
+        $this->assign($role);
+
+        $roleB->hasPermission($permission)->willReturn(false);
+        $role->hasPermission($permission)->willReturn(true);
+
+        $this->hasPermission($permission)->shouldReturn(true);
+    }
+
+    function it_has_no_permission_without_roles(Permission $permission)
+    {
+        $this->hasPermission($permission)->shouldReturn(false);
+    }
+
+    function it_has_no_permission_if_role_has_not(Role $role, Permission $permission)
+    {
+        $role->hasPermission($permission)->willReturn(false);
+
+        $this->assign($role);
+        $this->hasPermission($permission)->shouldReturn(false);
+    }
+
+    function it_checks_multiple_permissions_with_multiple_roles(Role $role, Permission $permission, Permission $permissionA, Role $roleB)
+    {
+        $role->hasPermission($permission)->willReturn(false);
+        $role->hasPermission($permissionA)->willReturn(true);
+
+        $roleB->hasPermission($permission)->willReturn(true);
+        $roleB->hasPermission($permissionA)->willReturn(false);
+
+        $role->equals($roleB)->willReturn(false);
+
+        $this->assign($role);
+        $this->assign($roleB);
+
+        $this->hasPermissions(array($permission, $permissionA))->shouldReturn(true);
+    }
+
+    function it_requires_all_multiple_permissions(Role $role, Permission $permission, Permission $permissionA, Role $roleB)
+    {
+        $role->hasPermission($permission)->willReturn(false);
+        $role->hasPermission($permissionA)->willReturn(true);
+
+        $roleB->hasPermission($permission)->willReturn(false);
+        $roleB->hasPermission($permissionA)->willReturn(true);
+
+        $role->equals($roleB)->willReturn(false);
+
+        $this->assign($role);
+        $this->assign($roleB);
+
+        $this->hasPermissions(array($permission, $permissionA))->shouldReturn(false);
+    }
+}
