323f73968e13bd92cfc0c6d679b0d6cc05bb8653
David Molineus <mail@netzmacht.de>
2014-11-17 08:35:14 +0100

    reduce form interface to required methods. Everything else are up to the implementation.

--- src/Netzmacht/Workflow/Form/Form.php
+++ src/Netzmacht/Workflow/Form/Form.php
@@ -22,61 +22,13 @@ use Netzmacht\Workflow\Flow\Context;
 interface Form
 {
     /**
-     * Validate form data.
+     * Validate form data and set form values as context params.
      *
      * @param Context $context The transition context.
      *
      * @return bool
      */
     public function validate(Context $context);
-
-    /**
-     * Render the form and return it as string.
-     *
-     * @return string
-     */
-    public function render();
-
-    /**
-     * Add label and description for a fieldset.
-     *
-     * @param string $name        Name of the fieldset.
-     * @param string $label       Label of the fieldset.
-     * @param string $description Description in the fieldset.
-     * @param string $class       Optional css class.
-     *
-     * @return $this
-     */
-    public function setFieldsetDetails($name, $label, $description = null, $class = null);
-
-    /**
-     * Create a form field.
-     *
-     * @param string      $name     Form field name.
-     * @param string      $type     Form field type.
-     * @param array       $extra    Form field extra configuration.
-     * @param string|null $fieldset Optional define a fieldset where to add the field. If null it's just created.
-     *
-     * @return FormField
-     */
-    public function createField($name, $type = 'text', array $extra = array(), $fieldset = null);
-
-    /**
-     * Add a field to the form.
-     *
-     * @param FormField $formField Formfield to be added.
-     * @param string    $fieldset  Name of the fieldset.
-     *
-     * @return $this
-     */
-    public function addField(FormField $formField, $fieldset = 'default');
-
-    /**
-     * Get form data of an validated form.
-     *
-     * @return array
-     */
-    public function getData();
 
     /**
      * Get errors of form.
