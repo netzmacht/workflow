020e9990ddabcf6cfaea36af2c5e37dcf9d21581
David Molineus <david.molineus@netzmacht.de>
2014-12-18 09:36:03 +0100

    Add missing file headers.

--- src/Netzmacht/Workflow/Flow/Transition.php
+++ src/Netzmacht/Workflow/Flow/Transition.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
 namespace Netzmacht\Workflow\Flow;
 
 use Netzmacht\Workflow\Data\ErrorCollection;
