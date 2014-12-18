abcded534e9b63c368ca352a4368d1a9b84cd4bf
David Molineus <david.molineus@netzmacht.de>
2014-12-18 09:37:41 +0100

    Fix package name.

--- src/Netzmacht/Workflow/Flow/State.php
+++ src/Netzmacht/Workflow/Flow/State.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @package    dev
+ * @package    workflow
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @copyright  2014 netzmacht creative David Molineus
  * @license    LGPL 3.0
