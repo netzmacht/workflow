62b251df15c3b56a0f10225ff3f7d15858f940a4
David Molineus <david.molineus@netzmacht.de>
2015-06-02 16:10:38 +0200

    Introduce a CachedManager.

---
+++ src/Netzmacht/Workflow/Manager/CachedManager.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * @package    workflow
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2015 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace Netzmacht\Workflow\Manager;
+
+use Netzmacht\Workflow\Data\EntityId;
+use Netzmacht\Workflow\Flow\Item;
+use Netzmacht\Workflow\Flow\Workflow;
+
+/**
+ * Workflow manager decorator caching the items and the relation between workflows and entities.
+ *
+ * @package Netzmacht\Workflow\Manager
+ */
+class CachedManager implements Manager
+{
+    /**
+     * @var Manager
+     */
+    private $manager;
+
+    /**
+     * Workflow entity mapping.
+     *
+     * @var array
+     */
+    private $workflows = array();
+
+    /**
+     * Cached workflow items.
+     *
+     * @var array
+     */
+    private $items = array();
+
+    /**
+     * Construct.
+     *
+     * @param Manager $manager The inside workflow manager.
+     */
+    public function __construct(Manager $manager)
+    {
+        $this->manager = $manager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Item $item, $transitionName = null)
+    {
+        return $this->handle($item, $transitionName);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addWorkflow(Workflow $workflow)
+    {
+        $this->manager->addWorkflow($workflow);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWorkflow(EntityId $entityId, $entity)
+    {
+        $key = $entityId->__toString();
+
+        if (!isset($this->workflows[$key])) {
+            $this->workflows[$key] = $this->getWorkflow($entityId, $entity);
+        }
+
+        return $this->workflows[$key];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWorkflowByName($name)
+    {
+        return $this->manager->getWorkflowByName($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWorkflowByItem(Item $item)
+    {
+        return $this->getWorkflow($item->getEntityId(), $item->getEntity());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasWorkflow(EntityId $entityId, $entity)
+    {
+        $key = $entityId->__toString();
+
+        if (isset($this->workflows[$key])) {
+            return true;
+        }
+
+        return $this->hasWorkflow($entityId, $entity);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getWorkflows()
+    {
+        return $this->manager->getWorkflows();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createItem(EntityId $entityId, $entity)
+    {
+        $key = $entityId->__toString();
+
+        if (!isset($this->items[$key])) {
+            $this->items[$key] = $this->manager->createItem($entityId, $entity);
+        }
+
+        return $this->items[$key];
+    }
+}
